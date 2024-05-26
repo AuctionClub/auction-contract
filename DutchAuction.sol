0ca9190d2d700079f05412f99feb66dcab3e45d0
agito <1634263395@qq.com>
2024-05-26 10:49:48 +0800

    update DutchAuction

--- DutchAuction.sol
+++ DutchAuction.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8//AutomationCompatible.sol";
 
 contract DutchAuction is Ownable, AutomationCompatibleInterface{
+
     struct Auction {
         address payable seller;
         address nftAddress;
@@ -14,37 +15,37 @@ contract DutchAuction is Ownable, AutomationCompatibleInterface{
         uint256 reservePrice;
         uint256 startTime;
         uint256 endTime;
-        uint256 deposit;
+        uint256 price_decay_interval;
+        uint256 price_decay_percentage;
+        uint256 reserve_duration;
         bool isActive;
     }
 
-    uint256 public constant FEE_PERCENTAGE = 4;
-    uint256 public constant DEPOSIT_PERCENTAGE = 10;
-    uint256 public constant PRICE_DECAY_INTERVAL = 3 minutes;
-    uint256 public constant PRICE_DECAY_PERCENTAGE = 5;
-    uint256 public constant RESERVE_DURATION = 5 minutes;
-
+    mapping(address nftAddress => mapping(uint256 tokenId => bool)) public isOnAuction;
     mapping(uint256 => Auction) public auctions;
     uint256 public auctionCount;
+    uint256 public constant FEE_PERCENTAGE = 3;   //手续费设置千分之三
 
     event AuctionStarted(uint256 indexed auctionId, address indexed seller, uint256 tokenId, uint256 startPrice, uint256 reservePrice, uint256 startTime);
     event AuctionEnded(uint256 indexed auctionId, address indexed buyer, uint256 finalPrice);
     event AuctionFailed(uint256 indexed auctionId);
     
-    // 构造函数，设置初始所有者
     constructor() Ownable(msg.sender) {}
 
-    // 开始拍卖的方法
+    /**
+    * 用户创建拍卖
+    */
     function startAuction(
         address nftAddress,
         uint256 tokenId,
         uint256 startPrice,
         uint256 reservePrice,
-        uint256 startTime
+        uint256 startTime,
+        uint256 price_decay_interval,
+        uint256 price_decay_amount,
+        uint256 reserve_duration
     ) external payable {
         require(startPrice > reservePrice, "Start price must be greater than reserve price");
-        uint256 deposit = (startPrice * DEPOSIT_PERCENTAGE) / 100;
-        require(msg.value == deposit, "Incorrect deposit amount");
 
         IERC721(nftAddress).approve(address(this), tokenId);
 
@@ -55,83 +56,79 @@ contract DutchAuction is Ownable, AutomationCompatibleInterface{
             startPrice: startPrice,
             reservePrice: reservePrice,
             startTime: startTime,
-            endTime: startTime + ((startPrice - reservePrice) / ((startPrice * PRICE_DECAY_PERCENTAGE) / 100)) * PRICE_DECAY_INTERVAL + RESERVE_DURATION,
-            deposit: deposit,
+            endTime: startTime + ((startPrice - reservePrice) / price_decay_amount) * price_decay_interval + reserve_duration,
             isActive: true
         });
         auctionCount++;
-
+        isOnAuction[nftAddress][tokenId] = true;
         emit AuctionStarted(auctionCount, msg.sender, tokenId, startPrice, reservePrice, startTime);
     }
 
-    // 获取当前价格的方法
+    /**
+    * 计算当前拍品的价格
+    */
     function getCurrentPrice(uint256 auctionId) public view returns (uint256) {
         Auction memory auction = auctions[auctionId];
         require(auction.isActive, "Auction is not active");
-        if (block.timestamp >= auction.endTime - RESERVE_DURATION) {
+        if (block.timestamp >= auction.endTime - auction.reserve_duration) {
             return auction.reservePrice;
         }
         uint256 elapsedTime = block.timestamp - auction.startTime;
-        uint256 decaySteps = elapsedTime / PRICE_DECAY_INTERVAL;
-        uint256 decayAmount = (auction.startPrice * PRICE_DECAY_PERCENTAGE * decaySteps) / 100;
-        uint currentPrice = auction.startPrice - decayAmount;
-        if (currentPrice <= auction.reservePrice) {
-            return auction.reservePrice;
-        }
+        uint256 decaySteps = elapsedTime / auction.price_decay_interval;
+        uint256 decayAmount = decaySteps * auction.price_decay_amount;
+        uint256 currentPrice = auction.startPrice - decayAmount;
         return currentPrice;
     }
     
-    // 竞拍的方法
+    /**
+    * 用户对指定NFT进行竞拍
+    */
     function bid(uint256 auctionId) external payable {
         Auction storage auction = auctions[auctionId];
         require(auction.isActive, "Auction is not active");
         uint256 currentPrice = getCurrentPrice(auctionId);
         require(msg.value >= currentPrice, "Bid amount is too low");
 
-        uint256 fee = (currentPrice * FEE_PERCENTAGE) / 100;
+        uint256 fee = (currentPrice * FEE_PERCENTAGE) / 1000;
         uint256 sellerProceeds = currentPrice - fee;
 
         auction.isActive = false;
         payable(owner()).transfer(fee); // 平台收取手续费
         auction.seller.transfer(sellerProceeds); // 卖家收到拍卖款项
-        auction.seller.transfer(auction.deposit); // 退还押金
         if (msg.value > currentPrice) {
             payable(msg.sender).transfer(msg.value - currentPrice); // 退还多余的ETH
         }
         
-        IERC721(auction.nftContract).transferFrom(auction.seller, msg.sender, auction.tokenId);
-
+        IERC721(auction.nftAddress).transferFrom(auction.seller, msg.sender, auction.tokenId);
         emit AuctionEnded(auctionId, msg.sender, currentPrice);
     }
 
-    // 用户终止拍卖
+    /**
+    * 用户取消拍卖
+    */
     function finalizeAuction(uint256 auctionId) external {
         Auction storage auction = auctions[auctionId];
         require(auction.isActive, "Auction is not active");
-        require(block.timestamp > auction.endTime, "Auction has not ended yet");
-
         auction.isActive = false;
-        
-        uint256 penalty = (auction.deposit * 10) / 100;
-        payable(owner()).transfer(penalty); // 平台没收10%押金
-        auction.seller.transfer(auction.deposit - penalty); // 退还剩余的押金
-
+        isOnAuction[auction.nftAddress][auction.tokenId] = false;
         emit AuctionFailed(auctionId);
     }
 
-    // 管理员终止拍卖
-    function withdrawDeposit(uint256 auctionId) external onlyOwner {
+    /**
+    * 拍卖时间截止自动结束拍卖
+    */
+    function withdrawDeposit(uint256 auctionId) internal {
         Auction storage auction = auctions[auctionId];
-        uint256 penalty = (auction.deposit * 10) / 100;
-        payable(owner()).transfer(penalty); // 平台没收10%押金
-        auction.seller.transfer(auction.deposit - penalty); // 退还剩余的押金
+        isOnAuction[auction.nftAddress][auction.tokenId] = false;
     }
 
+    /**
+    * chainlink自动化合约接口,检查是否有拍卖需要结束
+    */
     function checkUpkeep(bytes calldata checkData) external view override returns (bool upkeepNeeded, bytes memory performData) {
         upkeepNeeded = false;
         uint256 auctionId;
 
-        // 遍历拍卖ID列表，检查是否有拍卖需要结束
         for (uint256 i = 0; i < auctionCount; i++) {
             AuctionItem storage auction = auctions[i];
 
@@ -144,6 +141,9 @@ contract DutchAuction is Ownable, AutomationCompatibleInterface{
         }
     }
 
+    /**
+    * chainlink自动化合约接口,执行结束拍卖
+    */
     function performUpkeep(bytes calldata ) external { 
         uint256 auctionId = abi.decode(performData, (uint256));
         withdrawDeposit(auctionId);
